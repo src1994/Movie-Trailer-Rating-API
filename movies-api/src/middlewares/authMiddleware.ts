1f8cd6d834fa83107a94bf6119f2e6f9acd643ab
src1994 <sousamultimedia@gmail.com>
2024-10-12 18:13:35 +0100

    implement middleware

--- movies-api/src/middlewares/authMiddleware.ts
+++ movies-api/src/middlewares/authMiddleware.ts
@@ -1,4 +1,5 @@
-/*
+import { check } from 'express-validator';
+
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { Request, Response, NextFunction } from 'express';
@@ -9,16 +10,22 @@ dotenv.config();
 
 const SECRET_KEY = process.env.SECRET_KEY || ""; 
 
-
-export function checkRole(role: string) {
-    return  async (req: Request, res: Response, next: NextFunction) {
-        const token = req.headers.authorization?.split(" ")[1];
-        if (!token) { 
-            return res.status(401).json({ message: "Unauthorized" });
+export function checkRole(roles: string[]) {
+    return function(req: Request, res: Response, next: NextFunction) {
+        const  token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized. No token provided" })
         }
         try {
-            const  decoded = jwt.verify(token, process.env.SECRET_KEY as string);
-            if (!role.includes(decoded.role))
-       
+            const  decodedToken: any = jwt.verify(token,  SECRET_KEY);
+            if(!roles.includes(decodedToken.role)) {
+                return res.status(403).json({ message: "Acess Forbidden. User dosen't have required rule"} 
+                
+                )}
+            }catch (error) {
+                return res.status(403).json({error: "Access Forbidden. Invalid or  expired token"})
+            }
+            next();
+        }
+    
 }
-*/
