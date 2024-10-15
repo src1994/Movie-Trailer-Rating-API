149787e9106cd625b159a53b33c739af93f31b88
src1994 <sousamultimedia@gmail.com>
2024-10-15 15:39:40 +0100

    git ignore file

--- movies-api/dist/models/userModels.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, required: true, default: "USER" },
-    isActive: { type: Boolean, required: true, default: true },
-}, { timestamps: true });
-export default mongoose.model("User", UserSchema);
